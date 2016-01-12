8812f2f775ac6d8c5b47fc742d0bc301901eadc5
Thomas Krahn <thomas.krahn@esailors.de>
2016-01-12 14:25:35 +0100

    Optimize Tablespace scripts

--- tbs.sql
+++ tbs.sql
@@ -1,10 +1,26 @@
-SET lines 240 col tablespace_name FOR a30 col block_size FOR 99999 col max_size_gb FOR 9999 col contents FOR a12 col allocation_type for a10 col ssm for a10;
+COL "Tablespace" for a22
+COL "Used MB" for 99,999,999
+COL "Free MB" for 99,999,999
+COL "Total MB" for 99,999,999
+SET PAGES 100 LINES 80
 
-SELECT tablespace_name,
-  block_size,
-  round((max_size * block_size)/1024/1024/1024) AS max_size_gb,
-  contents, logging, allocation_type, segment_space_management as ssm
-FROM dba_tablespaces
-order by tablespace_name;
+SELECT df.tablespace_name "Tablespace",
+  totalusedspace "Used MB",
+  (df.totalspace - tu.totalusedspace) "Free MB",
+  df.totalspace "Total MB",
+  ROUND(100 * ( (df.totalspace - tu.totalusedspace)/ df.totalspace)) "Pct. Free"
+FROM
+  (SELECT tablespace_name,
+    ROUND(SUM(DECODE(autoextensible, 'YES', maxbytes, bytes)) / 1048576) TotalSpace
+  FROM dba_data_files
+  GROUP BY tablespace_name
+  ) df
+LEFT OUTER JOIN
+  (SELECT ROUND(SUM(bytes)/(1024*1024)) totalusedspace,
+    tablespace_name
+  FROM dba_segments
+  GROUP BY tablespace_name
+  ) tu
+ON(df.tablespace_name = tu.tablespace_name)
+ORDER BY df.tablespace_name;
 
-select
